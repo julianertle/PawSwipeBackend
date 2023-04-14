22e35ef8a18165bf39175c0c9e36c770c7c3b970
Julianertle <julian_ertle@web.de>
2023-04-14 14:44:57 +0200

    Added gitignore

--- tempDelme/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-}
