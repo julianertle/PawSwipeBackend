3241f1f9a3a555380eab4d1a586e62ac83da7824
Julianertle <julian_ertle@web.de>
2023-05-06 18:50:55 +0200

    Refactoring a bit

--- src/main/java/org/example/demo/Profile.java
+++ src/main/java/org/example/demo/Profile.java
@@ -12,156 +12,72 @@ public class Profile {
         return profile_id;
     }
 
-    public void setProfile_id(int profileId) {
-        this.profile_id = profileId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public byte[] getProfile_picture() {
-        return profile_picture;
     }
 
     public void setProfile_picture(byte[] profilePicture) {
         this.profile_picture = profilePicture;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public Date getCreation_date() {
-        return creation_date;
-    }
-
     public void setCreation_date(Date creationDate) {
         this.creation_date = creationDate;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public Date getBirthday() {
-        return birthday;
-    }
-
     public void setBirthday(Date birthday) {
         this.birthday = birthday;
-    }
-
-    public String getPhone_number() {
-        return phone_number;
     }
 
     public void setPhone_number(String phoneNumber) {
         this.phone_number = phoneNumber;
     }
 
-    public String getOpening_hours() {
-        return opening_hours;
-    }
-
     public void setOpening_hours(String openingHours) {
         this.opening_hours = openingHours;
-    }
-
-    public String getStreet() {
-        return street;
     }
 
     public void setStreet(String street) {
         this.street = street;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
     public void setCountry(String country) {
         this.country = country;
-    }
-
-    public String getCity() {
-        return city;
     }
 
     public void setCity(String city) {
         this.city = city;
     }
 
-    public int getStreet_number() {
-        return street_number;
-    }
-
     public void setStreet_number(int streetNumber) {
         this.street_number = streetNumber;
-    }
-
-    public String getHomepage() {
-        return homepage;
     }
 
     public void setHomepage(String homepage) {
         this.homepage = homepage;
     }
 
-    public int getPostal_code() {
-        return postal_code;
-    }
-
     public void setPostal_code(int postalCode) {
         this.postal_code = postalCode;
-    }
-
-    public String getDiscriminator() {
-        return discriminator;
     }
 
     public void setDiscriminator(String discriminator) {
         this.discriminator = discriminator;
     }
 
-    public int getIs_admin() {
-        return is_admin;
-    }
-
-    public void setIs_admin(int is_admin) {
-        this.is_admin = is_admin;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
     public void setFirstname(String firstname) {
         this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
     }
 
     public void setLastname(String lastname) {
